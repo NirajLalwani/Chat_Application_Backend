f7f25adb35b506d3fb7df514f7c98ff6f0d47a60
NirajLalwani <nirajlalwani2810@gmail.com>
2024-07-06 21:52:44 +0530

    added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const userRouter = require('./Routes/userRoutes');
 const server = require("http").createServer(app)
 const io = require('socket.io')(server, {
     cors: {
-        origin: process.env.CLIENT_URL
+        origin: "*"
     }
 }
 
